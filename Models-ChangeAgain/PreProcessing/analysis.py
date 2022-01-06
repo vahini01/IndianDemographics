172cfb44255dfa2de26e4f42e04b20f718a9b4e9
vahini01 <vahinimirididdi@gmail.com>
2022-01-06 05:23:17 +0000

    Remove files

--- Models-ChangeAgain/PreProcessing/analysis.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-def analyze_results(y_pred, ytrue):
-    #     Find where wrong, which surnames are most frequent - Use this algo, later can be updates 
-    #     Find where right, which surnames are most frequent
